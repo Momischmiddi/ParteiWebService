4de84fea6ed2f5dc66265b5a8ef7ca1dfc2a1d6e
momischmiddi <Moritzschmidt1995@gmail.com>
2020-06-18 20:27:47 +0200

    Trigger build

--- ParteiWebService/Startup.cs
+++ ParteiWebService/Startup.cs
@@ -22,8 +22,6 @@ namespace Aufgabe_2
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
