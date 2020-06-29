ac85301430aa41b44e0ad29747e500eb496b49f0
momischmiddi <Moritzschmidt1995@gmail.com>
2020-06-29 16:19:17 +0200

    Updated DB

--- ParteiWebService/Credentials.cs
+++ ParteiWebService/Credentials.cs
@@ -8,7 +8,7 @@ namespace ParteiWebService
 {
     public class Credentials
     {
-        public static string BlobServiceClientKey = "DefaultEndpointsProtocol=https;AccountName=cloudbobstorage;AccountKey=h2sMG0ISOmdJ+Ya7SF9YVqQ9e2Bk0RVeZOy4JwWmAIOA2oMtiqxOx05YjbNcmJpsDFVI8pykvtMcwO47xhkyWg==;EndpointSuffix=core.windows.net";
+        public static string BlobServiceClientKey = "DefaultEndpointsProtocol=https;AccountName=parteiblobstorage;AccountKey=ZqXo5wuejh6CR2xHNyD8McF7aTrhZEMBmKA/3vZqpAFR2ZAmC0pX3yF8VckgESCX6qCSLmfOQTbwAm1e8ejDcA==;EndpointSuffix=core.windows.net";
 
         public static string CosmosPasswordEvidence = "LwY1uc8q1gBKskWzATsVaZONh4xBP6WjGQuG5fH4qSGfYJYnHuxBqnF9HyyT8R2Kmm4WWmhk4Sdu80zgKHqPfw==";
     }
