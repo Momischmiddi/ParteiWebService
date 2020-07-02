fbfd81cf615fb251f012a4b757ba773a2cdcae41
momischmiddi <Moritzschmidt1995@gmail.com>
2020-07-02 12:23:04 +0200

    Added PDFGenerator URL

--- ParteiWebService/Credentials.cs
+++ ParteiWebService/Credentials.cs
@@ -11,5 +11,7 @@ namespace ParteiWebService
         public static string BlobServiceClientKey = "DefaultEndpointsProtocol=https;AccountName=cloudbobstorage;AccountKey=h2sMG0ISOmdJ+Ya7SF9YVqQ9e2Bk0RVeZOy4JwWmAIOA2oMtiqxOx05YjbNcmJpsDFVI8pykvtMcwO47xhkyWg==;EndpointSuffix=core.windows.net";
 
         public static string CosmosPasswordEvidence = "LwY1uc8q1gBKskWzATsVaZONh4xBP6WjGQuG5fH4qSGfYJYnHuxBqnF9HyyT8R2Kmm4WWmhk4Sdu80zgKHqPfw==";
+
+        public static string PDFMicroServiceUrl = "https://seniorenbobspdfservice.azurewebsites.net/PDFCreate/";
     }
 }
